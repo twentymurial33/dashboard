6026bbe390c7b23ca4c484e1746df8547a27176e
Murial Anindo <murial.anindo@lonett.com>
2022-04-19 19:40:54 -0400

    finally starting to understand,analytics routes to similar page

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import { Image, useTheme } from "@aws-amplify/ui-react";
-import background from "../assets/amazon.jpg";
 
 export function Head() {
   const { tokens } = useTheme();
 
   return (
-    <div style={{ backgroundImage: `url(${background})` }}>
-      <Image
-        src="../assets/amazon.jpg"
-        width="100%"
-        height="100%"
-        objectFit="cover"
-      />
+    <div>
+      <Image width="100%" height="100%" objectFit="cover" />
     </div>
   );
 }
