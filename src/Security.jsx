f2ff98ee759f58b6843da813395cdbc41292e637
Murial Anindo <murial.anindo@lonett.com>
2022-06-21 13:13:20 -0400

    changes to security and test

--- src/Security.jsx
+++ src/Security.jsx
@@ -2,8 +2,25 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Layout from "./components/Layout";
+import { embedDashboard } from "amazon-quicksight-embedding-sdk";
+
+const dashboard = embedDashboard(options);
 
 function Security() {
+  var containerDiv = document.getElementById("dashboardContainer");
+  var options = {
+    // url: "https://us-east-1.quicksight.aws.amazon.com/sn/dashboards/dashboardId?isauthcode=true&identityprovider=quicksight&code=authcode",
+    container: containerDiv,
+    parameters: {
+      country: "United States",
+    },
+    scrolling: "no",
+    height: "700px",
+    width: "1000px",
+  };
+  dashboard = QuickSightEmbedding.embedDashboard(options);
+  dashboard.on("error", onError);
+  dashboard.on("load", onDashboardLoad);
   return (
     <Layout>
       <Box
