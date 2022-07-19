bb706df1cafeaf9a94182305e38f2c68b70173aa
Murial Anindo <murial.anindo@lonett.com>
2022-07-18 22:19:41 -0400

    still on the lambda path

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,19 +1,60 @@
-import React, { useEffect } from "react";
+import Amplify, { API } from "aws-amplify";
 import Api from "@aws-amplify/api-rest";
+import React, { useEffect, useState } from "react";
 
-useEffect(()=>{
-    async function Dashboards(){
-        const userData= await Api.get('dashboardapi','/dashboards');//api name and endpoint
-        return userData.data.Items
-    }
-    fetchDashboards().then(()=>{
+const myAPI = "YOUR API HERE";
+const path = "/customers";
 
-    })
-},[]
+const App = () => {
+  const [input, setInput] = useState("");
+  const [customers, setCustomers] = useState([]);
 
+  //Function to fetch from our backend and update customers array
+  function getCustomer(e) {
+    let customerId = e.input;
+    API.get(myAPI, path + "/" + customerId)
+      .then((response) => {
+        console.log(response);
+        let newCustomers = [...customers];
+        newCustomers.push(response);
+        setCustomers(newCustomers);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-function Data() {
-  return <div></div>;
-}
+  return (
+    <div className="App">
+      <h1>Super Simple React App</h1>
+      <div>
+        <input
+          placeholder="customer id"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+      <br />
+      <button onClick={() => getCustomer({ input })}>
+        Get Customer From Backend
+      </button>
 
-export default Data;
+      <h2 style={{ visibility: customers.length > 0 ? "visible" : "hidden" }}>
+        Response
+      </h2>
+      {customers.map((thisCustomer, index) => {
+        return (
+          <div key={thisCustomer.customerId}>
+            <span>
+              <b>CustomerId:</b> {thisCustomer.customerId} - <b>CustomerName</b>
+              : {thisCustomer.customerName}
+            </span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;
